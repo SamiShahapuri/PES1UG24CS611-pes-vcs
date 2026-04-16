1d178c45b71ba5cbf150c0d0bfe33620af3dde3b
SamiShahapuri <samishahapuri313@gmail.com>
2026-04-16 09:29:43 -0400

    phase4-commit1: commit_create skeleton with tree building and parent read

--- commit.c
+++ commit.c
@@ -194,8 +194,15 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    // 1. Build tree from index
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0) return -1;
+
+    // 2. Read parent commit (if any)
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
+
+    // For now, just store dummy hash (will write commit in next step)
+    memset(commit_id_out->hash, 0, HASH_SIZE);
+    return 0;
 }
